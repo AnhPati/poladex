a5932c82274154bd8efab67d4f1f0889b4a4c682
Anh Pati <romain-felicianne@hotmail.fr>
2023-11-18 22:36:27 +0100

    Add icon lib
    Rajout du package react-icon de RadixUI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 'use client';
 import { Button } from '@radix-ui/themes'
+import { Header } from './components/Header';
 
 export default function Home() {
   return (
-    <div><h1>Hello world !</h1>
+    <div>
+      <Header />
+      <h1>Hello world !</h1>
       <Button>Click</Button>
     </div>
   )
