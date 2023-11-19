80a5d35b6e19d1f67ed709031b8a8fbc3518b879
Anh Pati <romain-felicianne@hotmail.fr>
2023-11-19 02:55:58 +0100

    Navigation link
    Création et stylisation de la navigation. Rendu dropdown menu ouvert.

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,22 +1,61 @@
 import { HamburgerMenuIcon } from "@radix-ui/react-icons"
+import { Heading } from "@radix-ui/themes"
+import Image from "next/image"
 import Link from "next/link"
+import styled from "styled-components"
+
+const DropdownContainer = styled.div`
+    position: absolute;
+    width: 100%;
+`
+
+const DropdownNav = styled.ul`
+    list-style-type: none;
+    padding: 0;
+    background: var(--amber-a10);
+    color: var(--amber-a12);
+    text-align: center;
+`
+
+const DropdownNavItem = styled.li`
+    height: 60px;
+    display: flex;
+    align-items: center;
+
+    > a {
+        width: 100%;
+        text-decoration: none;
+        color: var(--amber-a12);
+
+        &:hover {
+            color: var(--amber-a11);
+        }
+    }
+`
 
 export const Navigation = () => {
     return (
         <nav>
             <button>
-                <HamburgerMenuIcon />
+                <Image src={'/beers/hambuger-open.svg'} alt={'Poladex logo'} width={20} height={20} />
             </button>
-            <div>
-                <ul>
-                    <li>
-                        <Link href='/authentification'>Authentification</Link>
-                    </li>
-                    <li>
-                        <Link href={'/poladex'}>Poladex</Link>
-                    </li>
-                </ul>
-            </div>
+            <DropdownContainer>
+                <DropdownNav>
+                    <DropdownNavItem>
+                        <Link href={'/authentification'}>
+                            <Heading as={'h2'} weight={'medium'}>
+                                Authentification
+                            </Heading>
+                        </Link>
+                    </DropdownNavItem>
+                    <DropdownNavItem>
+                        <Link href={'/poladex'}>
+                            <Heading as={'h2'} weight={'medium'}>
+                                Poladex
+                            </Heading></Link>
+                    </DropdownNavItem>
+                </DropdownNav>
+            </DropdownContainer>
         </nav>
     )
 }
