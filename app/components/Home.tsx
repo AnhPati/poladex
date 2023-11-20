71f5cc3f296bf5be2b423001ccf7d8292ebe26c5
Anh Pati <romain-felicianne@hotmail.fr>
2023-11-20 18:15:00 +0100

    Home button
    Création d'un composant Home pour le rendu coté client séparé du rendu coté serveur dont j'ai besoin dans la page de l'App pour vérifié l'utilisateur connecté dans la session.
    Affichage conditionnel du bouton qui mène au pokédex.

---
+++ app/components/Home.tsx
@@ -0,0 +1,86 @@
+'use client'
+
+import { Button, Flex, Heading, Text } from '@radix-ui/themes'
+import Image from 'next/image'
+import Link from 'next/link'
+import styled from 'styled-components'
+import { LoginButton } from './authentification/LoginButton'
+
+const HeadingContainer = styled.div`
+  width: 180px;
+  text-align: center;
+`
+
+const TextContainer = styled.div`
+  width: 280px;
+  margin-top: 40px;
+  text-align: center;
+`
+
+const ButtonContainer = styled.div`
+  button {
+    width: 140px;
+    height: 50px;
+    text-align: center;
+  }
+    a {
+      font-weight: 600;
+    }
+`
+
+export const Home = ({ user }: { user?: {} }) => {
+    return (
+        <Flex
+            direction={'column'}
+            align={'center'}
+        >
+            <HeadingContainer>
+                <Heading
+                    as={'h2'}
+                    mt={'6'}
+                    mb={'6'}
+                    color={'amber'}
+                    highContrast
+                >
+                    {'Bienvenue sur'.toUpperCase()}
+                </Heading>
+            </HeadingContainer>
+            <Image
+                src={'/poladex_logo-txt.svg'}
+                alt={'Poladex logo'}
+                width={360}
+                height={360}
+            />
+            <TextContainer>
+                <Text
+                    color={'amber'}
+                    highContrast
+                >
+                    {'L’application qui permet de garder un souvenir des bières les plus exotiques que l’on a pu boire ...'.toUpperCase()}
+                </Text>
+            </TextContainer>
+            <HeadingContainer>
+                <Heading
+                    as={'h2'}
+                    mt={'6'}
+                    mb={'6'}
+                    color={'amber'}
+                    highContrast
+                >
+                    {`Dégustez les toutes !`.toUpperCase()}
+                </Heading>
+            </HeadingContainer>
+            <ButtonContainer>
+                {user ? (
+                    <Button>
+                        <Link href={'/poladex'}>
+                            {'Accéder au Poladex'.toUpperCase()}
+                        </Link>
+                    </Button>
+                ) : (
+                    <LoginButton />
+                )}
+            </ButtonContainer>
+        </Flex>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,82 +1,10 @@
-'use client'
+import { Home } from "./components/Home"
+import { getAuthSession } from "./lib/auth-options"
 
-import { Button, Flex, Heading, Text } from '@radix-ui/themes'
-import Image from 'next/image'
-import Link from 'next/link'
-import styled from 'styled-components'
+export default async function App() {
+  const session = await getAuthSession()
 
-const HeadingContainer = styled.div`
-  width: 180px;
-  text-align: center;
-`
-
-const TextContainer = styled.div`
-  width: 280px;
-  margin-top: 40px;
-  text-align: center;
-`
-
-const ButtonContainer = styled.div`
-  button {
-    width: 140px;
-    height: 50px;
-    text-align: center;
-  }
-    a {
-      font-weight: 600;
-    }
-`
-
-export default function Home() {
   return (
-    <Flex
-      direction={'column'}
-      align={'center'}
-    >
-      <HeadingContainer>
-        <Heading
-          as={'h2'}
-          mt={'6'}
-          mb={'6'}
-          color={'amber'}
-          highContrast
-        >
-          {'Bienvenue sur'.toUpperCase()}
-        </Heading>
-      </HeadingContainer>
-      <Image
-        src={'/poladex_logo-txt.svg'}
-        alt={'Poladex logo'}
-        width={360}
-        height={360}
-      />
-      <TextContainer>
-        <Text
-          color={'amber'}
-          highContrast
-        >
-          {'L’application qui permet de garder un souvenir des bières les plus exotiques que l’on a pu boire ...'.toUpperCase()}
-        </Text>
-      </TextContainer>
-      <HeadingContainer>
-        <Heading
-          as={'h2'}
-          mt={'6'}
-          mb={'6'}
-          color={'amber'}
-          highContrast
-        >
-          {`Dégustez les
-          toutes !`.toUpperCase()}
-        </Heading>
-      </HeadingContainer>
-      <ButtonContainer>
-        <Button>
-          <Link href={'/poladex'}>
-            {'Accéder au Poladex'.toUpperCase()}
-          </Link>
-        </Button>
-      </ButtonContainer>
-    </Flex>
+    <Home user={session?.user} />
   )
 }
