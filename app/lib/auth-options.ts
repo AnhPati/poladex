6ca362d2e60da70984d6f625851c900af2774534
Anh Pati <romain-felicianne@hotmail.fr>
2023-11-20 01:57:29 +0100

    Magic Link
    Tentative de set up d'un mail magique pour la connexion.
    Installation de mailDev et nodemailer. Configuration de NextAuth.

--- app/lib/auth-options.ts
+++ app/lib/auth-options.ts
@@ -1,8 +1,10 @@
 import GoogleProvider from "next-auth/providers/google";
+import Email from "next-auth/providers/email";
 import { env } from "./env";
 import { AuthOptions } from 'next-auth'
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "./prisma";
+
 
 export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
@@ -10,6 +12,17 @@ export const authOptions: AuthOptions = {
         GoogleProvider({
             clientId: env.GOOGLE_ID,
             clientSecret: env.GOOGLE_SECRET
-        })
+        }),
+        Email({
+            from: process.env.EMAIL_FROM,
+            server: {
+                host: process.env.SMTP_HOST,
+                port: Number(process.env.SMTP_PORT),
+                auth: {
+                    user: process.env.SMTP_USER,
+                    pass: process.env.SMTP_PASSWORD,
+                },
+            }
+        }),
     ]
 }
